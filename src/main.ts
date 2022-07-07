2aeaf79143d658eb6218095da33b4252c68279bf
Inwebo <github@inwebo.net>
2022-07-07 09:26:40 +0200

    Enable versionning

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +9,12 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  app.setGlobalPrefix('v1');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
